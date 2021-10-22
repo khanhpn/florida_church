ed6d25bbafeffe4c85fd21f0a2627a6ca23fa29c
khanhpn <phamkhanh90@gmail.com>
2021-10-22 14:48:09 +0700

    base project redmine admin page

--- app/javascript/packs/admin/dashboard2.js
+++ app/javascript/packs/admin/dashboard2.js
@@ -119,15 +119,15 @@ $(function () {
    * ------------
    * Create a world map with markers
    */
-  $('#world-map-markers').mapael({
-    map: {
-      name: 'usa_states',
-      zoom: {
-        enabled: true,
-        maxLevel: 10
-      }
-    }
-  })
+  // $('#world-map-markers').mapael({
+  //   map: {
+  //     name: 'usa_states',
+  //     zoom: {
+  //       enabled: true,
+  //       maxLevel: 10
+  //     }
+  //   }
+  // })
 
   // $('#world-map-markers').vectorMap({
   //   map              : 'world_en',
