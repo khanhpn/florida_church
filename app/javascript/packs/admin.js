ec8760c87a4b5dcf62803bd862e46ca31307eeb2
khanhpn <phamkhanh90@gmail.com>
2021-10-22 16:32:37 +0700

    base project redmine admin page

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -8,13 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 require("jquery");
 require("raphael");
-// require("jquery-mousewheel");
-//require("jquery-mapael");
 require("packs/admin/jquery.min.js");
 require("packs/admin/bootstrap.bundle.min.js");
 require("packs/admin/jquery.overlayScrollbars.min.js");
 require("packs/admin/adminlte.js");
-require("packs/admin/usa_states.min.js");
 require("packs/admin/Chart.min.js");
 require("packs/admin/demo.js");
 require("packs/admin/dashboard2.js");
