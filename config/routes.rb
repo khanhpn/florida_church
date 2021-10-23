0ef1cdbd092b531c9826306f18e0284a09e69e90
khanhpn <phamkhanh90@gmail.com>
2021-10-23 16:09:35 +0700

    base project redmine admin page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    get "dashboard", to: 'dashboard#index'
+    get "/", to: 'dashboard#index'
     resources :introduction, except: :show
     resources :mass
     resources :users
