2b00840637f00497266c3716612cd75d3bffcd5a
khanhpn <phamkhanh90@gmail.com>
2022-09-16 07:34:44 +0700

    refactor production js to improve performance

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,25 +1,7 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
-require("jquery");
 
 Rails.start();
 Turbolinks.start();
 
-require("packs/library/hammer.min.js");
-require("packs/library/photorotator");
-require("packs/library/moment");
-require("packs/library/moment-timezone-with-data-2012-2022");
-require("packs/library/jquery.textfill.min");
-require("packs/library/jquery.focuspoint");
-require("packs/library/photoswipe-init");
-require("packs/library/photoswipe");
-require("packs/library/photoswipe-ui-default");
 require("packs/library/production");
-require("packs/library/map");
-require("packs/library/lightbox");
-require("packs/library/custom");
