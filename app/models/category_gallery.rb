780b3d4828da5fc2729aa5bd308226cdab29a35f
khanhpn <phamkhanh90@gmail.com>
2022-09-19 07:29:00 +0700

    implement category photo

--- app/models/category_gallery.rb
+++ app/models/category_gallery.rb
@@ -1,4 +1,9 @@
 class CategoryGallery < ApplicationRecord
   has_many :photo_galleries, dependent: :destroy
   belongs_to :user
+
+  has_one_attached :file_image
+
+  validates :link_google_photo, presence: true, if: -> { enable_google_photo }
+  validates :name, presence: true
 end
