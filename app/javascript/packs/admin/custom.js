c3e88692c3cb2975d31d09c54aee68bf16c02634
khanhpn <phamkhanh90@gmail.com>
2022-09-19 19:05:55 +0700

    implement category photo

--- app/javascript/packs/admin/custom.js
+++ app/javascript/packs/admin/custom.js
@@ -8,7 +8,5 @@ $(document).on("click", "#id_enable_google_photo", function () {
     $("#id_photo_google_link").show();
   } else {
     $("#id_photo_google_link").hide();
-    $("#id_google_photo_text_field").val("");
-    $("#id_google_photo_file_field").val("");
   }
 });
