66db9ce1cf23479a6419652f8420770fd9f00d3c
khanhpn <phamkhanh90@gmail.com>
2022-09-19 07:30:29 +0700

    implement category photo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,27 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @masses = Rails.cache.fetch("cache_normal_mass", expires_in: 15.minutes) do
-      Mass.normal_mass.last(4)
-    end
-
-    @mass_ticked = Rails.cache.fetch("cache_normal_mass_ticked", expires_in: 15.minutes) do
-      Mass.ticked_mass.last
-    end
-
-    @mass_time = Rails.cache.fetch("cache_normal_mass_time", expires_in: 15.minutes) do
-      MassTime.last
-    end
-
-    @adoration_time = Rails.cache.fetch("cache_adoration_time", expires_in: 15.minutes) do
-      AdorationTime.last
-    end
-
-    @special_note = Rails.cache.fetch("cache_special_note", expires_in: 15.minutes) do
-      SpecialNote.last
-    end
-
-    @gallery_image = Rails.cache.fetch("cache_gallery_image", expires_in: 1.hours) do
-      GalleryImage.display_order_top.first.contents
-    end
+    @masses = Mass.normal_mass.last(4)
+    @mass_ticked = Mass.ticked_mass.last
+    @mass_time = MassTime.last
+    @adoration_time = AdorationTime.last
+    @special_note = SpecialNote.last
+    @gallery_image = GalleryImage.display_order_top.first
   end
 end
